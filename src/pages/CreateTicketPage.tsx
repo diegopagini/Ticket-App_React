14b96bcef8f05e844da045b719d6610dc16527ef
Diego Pagini <diego.pagini@neoris.com>
2023-03-13 09:23:48 +0100

    create ticket page

--- src/pages/CreateTicketPage.tsx
+++ src/pages/CreateTicketPage.tsx
@@ -1,5 +1,62 @@
 /** @format */
+import { DownloadOutlined } from '@ant-design/icons';
+import { Button, Col, Row, Typography } from 'antd';
+
+const { Title, Text } = Typography;
 
 export const CreateTicketPage = () => {
-	return <div>CreateTicket</div>;
+	const newTicket = () => {
+		console.log('newTicket');
+	};
+
+	return (
+		<>
+			<Row>
+				<Col
+					span={14}
+					offset={6}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						flexDirection: 'column',
+					}}
+				>
+					<Title level={3}>Press button to new ticket</Title>
+					<Button
+						type='primary'
+						shape='round'
+						icon={<DownloadOutlined />}
+						size='large'
+						onClick={newTicket}
+					>
+						New ticket
+					</Button>
+				</Col>
+			</Row>
+			<Row style={{ marginTop: 100 }}>
+				<Col
+					span={14}
+					offset={6}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						flexDirection: 'column',
+					}}
+				>
+					<Text>Your number</Text>
+					<br />
+					<Text
+						type='success'
+						style={{
+							fontSize: 55,
+						}}
+					>
+						5
+					</Text>
+				</Col>
+			</Row>
+		</>
+	);
 };
